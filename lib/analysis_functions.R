af2d39d69946ebc5ac24a605d15aca8c3425d347
oferisak <doctoroots@gmail.com>
2024-08-21 09:03:55 +0300

    working on the baysian approach for prob calculation

--- lib/analysis_functions.R
+++ lib/analysis_functions.R
@@ -12,8 +12,6 @@ produce_roc<-function(scores_table,tool){
   return(proc)
 }
 
-
-
 combine_filter_sets<-function(var_sets, var_set_names) {
   if (!all(var_set_names %in% names(var_sets))) {
     stop("One or more specified elements do not exist in the list")
@@ -25,10 +23,161 @@ combine_filter_sets<-function(var_sets, var_set_names) {
   return(result)
 }
 
+calculate_calibration_df<-function(tool_var_set,tool,breaks=10,hmisc=FALSE,prior_prob=0.0441,round_score_val=3){
+  scores<-tool_var_set%>%pull(tool)%>%round(round_score_val)%>%unique()
+  # Generate all combinations of two elements
+  combinations <- t(combn(scores, 2))
+  # Filter combinations where the first value is less than the second
+  valid_combinations <- data.frame(combinations[combinations[, 1] < combinations[, 2], ])
+  prior_odds<-prior_prob/(1-prior_prob)
+  cumevents<-tool_var_set%>%select(tool,clinvar_class)%>%
+    mutate(round_tool=round(!!sym(tool),round_score_val))%>%
+    arrange(-round_tool)
+  cumevents$cum_event<-cumsum(cumevents$clinvar_class=='P/LP')
+  cumevents$total_at_or_above_score<-1:nrow(cumevents)
+  cumevents<-cumevents%>%
+    group_by(round_tool)%>%
+    slice_max(total_at_or_above_score)%>%
+    ungroup()%>%select(-clinvar_class)
+
+  weight<-(sum(tool_var_set$clinvar_class=='P/LP')/sum(tool_var_set$clinvar_class=='B/LB'))/prior_odds
+  valid_combinations<-valid_combinations%>%left_join(cumevents,by=c('X1'='round_tool'))%>%
+    left_join(cumevents,by=c('X2'='round_tool'))
+  valid_combinations<-valid_combinations%>%
+    mutate(events_in_range=cum_event.x-cum_event.y,
+           total_in_range=total_at_or_above_score.x-total_at_or_above_score.y)
+  valid_combinations<-
+    valid_combinations%>%
+    filter(total_in_range>300,total_in_range<30000)%>%
+    mutate(tool = tool,
+           # likelihood = events_in_range / sum(events_in_range),
+           # p_score = total_in_range / sum(total_in_range),
+           # posterior_prob = (likelihood * prior_prob) / p_score,
+           posterior_prob_weight = events_in_range / (events_in_range + (total_in_range - events_in_range) * weight),
+           
+           # binom_ci = purrr::map2(events_in_range, round(events_in_range + (total_in_range - events_in_range) * weight),
+           #                        ~ binom.test(.x, .y, conf.level = 0.95)$conf.int)
+    ) #%>%
+    # mutate(low_ci = purrr::map_dbl(binom_ci, 1),
+    #        high_ci = purrr::map_dbl(binom_ci, 2)) %>%
+    # select(-binom_ci)
+    # z<-valid_combinations%>%mutate(binom_ci = purrr::map2_df(total_in_range,round(posterior_prob_weight*total_in_range),~binom.test(n=.x,x=.y)$conf.int))#%>%
+    # z%>% mutate(low_ci = binom_ci[1],high_ci=binom_ci[2]) %>%
+    #          select(-binom_ci)
+  
+    class_cats<-c('VS','S','M','P')
+    class_posteriors<-c(0.9811,0.6073,0.2108,0.0999)
+    ranges<-NULL
+    remaining_combinations<-valid_combinations
+    for (class_post in class_posteriors){
+      print(class_post)
+      matching_ranges<-remaining_combinations%>%
+        #filter(abs(class_post-posterior_prob_weight)==min(abs(class_post-posterior_prob_weight)))
+        filter(((abs(class_post-posterior_prob_weight)<0.002)|
+                  ((posterior_prob_weight-class_post)>0.002)&((posterior_prob_weight-class_post)<0.01)))%>%
+        slice_max(total_in_range)
+      ranges<-ranges%>%bind_rows(data.frame(class_post,
+                                            matching_ranges))
+      remaining_combinations<-valid_combinations%>%filter(X2==matching_ranges$X1)
+    }
+    ranges<-ranges%>%
+      bind_rows(valid_combinations%>%filter(X1==max(ranges$X2),X2==max(valid_combinations$X2)))%>%
+      arrange(-X1)%>%mutate(class_post=c(class_posteriors,NA),class_cats=c(class_cats,NA))%>%
+      select(-c(cum_event.x,cum_event.y,total_at_or_above_score.x,total_at_or_above_score.y))
+    
+  if (hmisc){
+    to_ret<-tool_var_set%>%mutate(.pred_cat=Hmisc::cut2(!!sym(tool),m = 500))
+  }else{
+    to_ret<-tool_var_set%>%mutate(.pred_cat=cut(!!sym(tool),breaks = breaks))
+  }
+  to_ret <-
+    to_ret %>%
+    group_by(.pred_cat) %>%
+    summarize(total = n(),
+              events = sum(clinvar_class == 'P/LP'),
+              rate = events / total) %>%
+    mutate(tool = tool,
+           likelihood = events / sum(events),
+           p_score = total / sum(total),
+           posterior_prob = (likelihood * prior_prob) / p_score,
+           posterior_prob_weight = events / (events + (total - events) * weight),
+           binom_ci = purrr::map2(events, round(events + (total - events) * weight),
+                           ~ binom.test(.x, .y, conf.level = 0.95)$conf.int)
+    ) %>%
+    mutate(low_ci = purrr::map_dbl(binom_ci, 1),
+           high_ci = purrr::map_dbl(binom_ci, 2)) %>%
+    select(-binom_ci)
+  tail(to_ret)
+  
+  return(to_ret)
+}
+
+
+# claude solution for posterior prob calculation
+calculate_plp_probability <- function(scores, labels, dataset_ratio, pop_ratio) {
+  # Ensure labels are factor
+  labels <- factor(labels, levels = c("P/LP", "B/LB"))
+  
+  # Calculate likelihoods
+  density_plp <- density(scores[labels == "P/LP"])
+  density_blb <- density(scores[labels == "B/LB"])
+  
+  # Function to get density at a specific point
+  get_density <- function(x, d) {
+    idx <- which.min(abs(d$x - x))
+    return(d$y[idx])
+  }
+  
+  # Calculate posterior probabilities
+  posterior_probs <- sapply(scores, function(score) {
+    likelihood_plp <- get_density(score, density_plp)
+    likelihood_blb <- get_density(score, density_blb)
+    
+    # Using dataset_ratio as prior
+    prior_plp <- dataset_ratio
+    prior_blb <- 1 - dataset_ratio
+    
+    posterior_plp <- (likelihood_plp * prior_plp) / 
+      (likelihood_plp * prior_plp + likelihood_blb * prior_blb)
+    
+    # Adjust for population ratio
+    adjusted_posterior_plp <- (posterior_plp * pop_ratio) / 
+      (posterior_plp * pop_ratio + (1 - posterior_plp) * (1 - pop_ratio))
+    
+    return(adjusted_posterior_plp)
+  })
+  
+  return(posterior_probs)
+}
+
+calculate_dataset_posterior_prob<-function(tool_var_set,tool,original_dataset_ratio,pop_ratio=0.0441){
+  data <- tool_var_set
+  
+  # Calculate dataset-specific ratio
+  dataset_ratio <- mean(data$clinvar_class == "P/LP")
+  
+  # adjust pop_ratio according to the dataset ratio 
+  adjusted_pop_ratio<- pop_ratio * (dataset_ratio/original_dataset_ratio)
+  print(adjusted_pop_ratio)
+  
+  probabilities <- calculate_plp_probability(
+    data%>%pull(tool), 
+    data%>%pull(clinvar_class), 
+    dataset_ratio, 
+    adjusted_pop_ratio
+  )
+  criteria<-cut(probabilities,breaks=c(0.0999,0.2108,0.6073,0.9811,1),labels=c('P','M','S','VS'))
+  return(data.frame(tool,score=data%>%pull(tool),post_prob = probabilities,criteria))
+}
+z<-calculate_dataset_posterior_prob(tool_var_set,tool,original_dataset_ratio,pop_ratio = 0.0441)
+z%>%group_by(criteria)%>%slice_min(score,with_ties = F)
+z%>%count(criteria)
+
 calculate_roc_metrics<-function(proc_data,var_sets_list,tools_to_test,complete=TRUE,save_rocs=TRUE){
   roc_metrics_table<-NULL
   rocs_table<-NULL
   procs<-list()
+  original_dataset_ratio<-mean(proc_data$clinvar_class=='P/LP',na.rm = T)
   for (i in 1:nrow(var_sets_list)){
     row<-var_sets_list[i,,drop=F]
     message(glue('{i}/{nrow(var_sets_list)}:analyzing {paste0(row%>%slice(1),collapse=":")}'))
@@ -109,6 +258,8 @@ calculate_roc_metrics<-function(proc_data,var_sets_list,tools_to_test,complete=T
     to_ret<-list()
     to_ret[['procs']]<-procs
     to_ret[['roc_metrics_table']]<-roc_metrics_table
+    to_ret[['calibration_df']]<-calculate_calibration_df(tool_var_set,tool,breaks=10)
+    to_ret[['calibration_df_hmisc']]<-calculate_calibration_df(tool_var_set,tool,breaks=10,hmisc = TRUE)
   }
   return(to_ret)
 }
